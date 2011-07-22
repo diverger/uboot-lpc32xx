3464510e644c8814b6f62c1459b356c6a9530f51
diverger <i51211314@gmail.com>
2011-07-22 17:25:19 +0800

    Prepare to port to smartarm3250 board, named "smart3250".
    Signed-off-by: diverger <i51211314@gmail.com>

---
+++ board/smart3250/net.c
@@ -0,0 +1,577 @@
+/*
+ * Copyright (C) 2008 by NXP Semiconductors
+ * All rights reserved.
+ *
+ * @Author: Kevin Wells
+ * @Descr: Phytec 3250 net interface support functions
+ *
+ * See file CREDITS for list of people who contributed to this
+ * project.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+#include <common.h>
+#include <command.h>
+#include <net.h>
+#include <lpc3250.h>
+#include <configs/smart3250.h>
+#include "smart3250_prv.h"
+#include "miiphy.h"
+
+static unsigned long g_dmabase;
+static unsigned long gdma_size;
+static TXRX_DESC_T *pTXDesc;
+static unsigned long *pTXStatus;
+static unsigned long pTXVBuffs [ENET_MAX_TX_PACKETS];
+static TXRX_DESC_T *pRXDesc;
+static RX_STATUS_T *pRXStatus;
+static unsigned long pRXVBuffs [ENET_MAX_RX_PACKETS];
+#ifndef USE_IRAM_FOR_ETH_BUFFERS
+static unsigned long ethbuffs[4096];
+#endif
+extern PHY_HW_T phyhwdesc;
+
+static void msDelay(unsigned long ms)
+{
+	udelay(ms * 1000);
+}
+
+//------------------------------------------------------------------------------
+static int RMII_Write (unsigned long PhyReg, unsigned long Value)
+{
+	unsigned long mst = 250;
+	int sts = 0;
+
+	// Write value at PHY address and register
+	ENETMAC->madr = (PHYDEF_PHYADDR << 8) | PhyReg;
+	ENETMAC->mwtd = Value;
+
+	// Wait for unbusy status
+	while (mst > 0)
+	{
+		if ((ENETMAC->mind & MIND_BUSY) == 0)
+		{
+			mst = 0;
+			sts = 1;
+		}
+		else
+		{
+			mst--;
+			msDelay(1);
+		}
+	}
+
+	return sts;
+}
+
+//------------------------------------------------------------------------------
+int RMII_Read(unsigned long PhyReg, unsigned long *data) 
+{
+	unsigned long mst = 250;
+	int sts = 0;
+
+	// Read value at PHY address and register
+	ENETMAC->madr = (PHYDEF_PHYADDR << 8) | PhyReg;
+	ENETMAC->mcmd = MCMD_READ;
+
+	// Wait for unbusy status
+	while (mst > 0)
+	{
+		if ((ENETMAC->mind & MIND_BUSY) == 0)
+		{
+			mst = 0;
+			*data = ENETMAC->mrdd;
+			sts = 1;
+		}
+		else
+		{
+			mst--;
+			msDelay(1);
+		}
+	}
+
+	ENETMAC->mcmd = 0;
+
+	return sts;
+}
+
+int phy_reset(void)
+{
+	int goodacc;
+	unsigned long temp, mst;
+	
+	// Reset the PHY and wait for reset to complete
+	goodacc = RMII_Write(PHY_REG_BMCR, PHY_BMCR_RESET_BIT);
+
+	if (goodacc == 0)
+	{
+		// not good
+		return 0;
+	}
+
+	// most try 400
+	mst = 400;
+
+	goodacc = 0;
+	while (mst > 0)
+	{
+		RMII_Read(PHY_REG_BMCR, &tmp);
+		if ((tmp & PHY_BMCR_RESET_BIT) == 0)
+		{
+			mst = 0;
+			goodacc = 1;
+		}
+		else
+		{
+			mst--;
+			msDelay(1);
+		}
+	}
+
+	return goodacc;
+}
+
+static int phy_get_link_status (void)
+{
+	unsigned long status;
+	int mst = 5000;
+	int goodacc = 1;
+	int bcomplete = 0;
+
+	/* Status is read once to clear old link state */
+	RMII_Read(PHY_BMSR,&status);
+
+	/*
+	 * Wait if the link is up, and auto-negotiation is in progress
+	 * (ie - we're capable and it's not done)
+	 */
+	status = 0;
+	goodacc &= RMII_Read(PHY_REG_BMSR, &status);
+	
+	while ( (status & PHY_BMSR_LINKUP_STATUS)		// link is up
+		&& (status & PHY_BMSR_AUTON_ABLE)			// auto-negotiation enabled
+		&& !(status & PHY_BMSR_AUTON_COMPLETE)		// auto-negotiation not complete
+		&& ( mst > 0 )								// not timeout yet 
+		)
+	{
+		goodacc &= RMII_Read(PHY_REG_BMSR, &status);
+		--mst;
+		msDelay(1);		// delay 1ms		
+	}
+
+	if ( status & PHY_BMSR_AUTON_COMPLETE )
+	{
+		// auto negotiation complete
+		printf( "ENET:auto-negotiation complete.\n");
+		return 0;
+	}
+	else if ( status & PHY_BMSR_LINKUP_STATUS )
+	{
+		// link is up
+		return 0;
+	}
+	else
+	{
+		// auto-negotiation timeout, or not enabled, or link isn't up
+		return 1;
+	}
+	
+	return 1;
+}
+
+//------------------------------------------------------------------------------
+int txrx_setup(void)
+{
+	int idx;
+	unsigned long *pTXStatusL, pbase1, pbase2, pbase3;
+	TXRX_DESC_T *pTXRXDesc;
+	RX_STATUS_T *pRXStatusL;
+
+	// Get physical address and size of DMA buffers
+#ifdef USE_IRAM_FOR_ETH_BUFFERS
+	g_dmabase = (unsigned long) IRAM_ETH_BUFF_BASE;
+	gdma_size = IRAM_ETH_BUFF_SIZE;
+#else
+	g_dmabase = (unsigned long) &ethbuffs;
+	gdma_size = sizeof(ethbuffs);
+#endif
+
+	// Setup base pointers
+	pbase1 = g_dmabase;     // Start of descriptors
+	pbase2 = pbase1 + 256;  // Start of statuses
+	pbase3 = pbase1 + 1024; // Start of buffers
+
+	// Setup pointers to TX structures
+	ENETMAC->txdescriptor =  pbase1;
+	ENETMAC->txstatus = pbase2;
+	ENETMAC->txdescriptornumber = (ENET_MAX_TX_PACKETS - 1);
+
+	// Save base address of TX descriptor table and TX status
+	pTXRXDesc = (TXRX_DESC_T *) pbase1;
+	pTXStatusL = (unsigned long *) pbase2;
+	pTXDesc = pTXRXDesc;
+	pTXStatus = pTXStatusL;
+
+	// Build TX descriptors
+	for (idx = 0; idx < ENET_MAX_TX_PACKETS; idx++)
+	{
+		pTXRXDesc->packet = pbase3;
+		pTXRXDesc->control = 0;
+		*pTXStatusL = 0;
+
+		// Save virtual address of buffer
+#ifdef USE_IRAM_FOR_ETH_BUFFERS
+		pTXVBuffs [idx] = (unsigned long) pbase3;
+#else
+#pragma error "NOT supported"
+#endif
+
+		// Next descriptor and status
+		pTXRXDesc++;
+		pTXStatusL++;
+		pbase1 += sizeof (TXRX_DESC_T);
+		pbase2 += sizeof (unsigned long);
+		pbase3 += ENET_MAXF_SIZE;
+	}
+
+	// Setup pointers to RX structures
+	ENETMAC->rxdescriptor = pbase1;
+	ENETMAC->rxstatus = pbase2;
+	ENETMAC->rxdescriptornumber = (ENET_MAX_RX_PACKETS - 1);
+
+	// Save base address of RX descriptor table and RX status
+	pRXDesc = pTXRXDesc;
+	pRXStatus = pRXStatusL = (RX_STATUS_T *) pTXStatusL;
+
+	// Build RX descriptors
+	for (idx = 0; idx < ENET_MAX_TX_PACKETS; idx++)
+	{
+		pTXRXDesc->packet = pbase3;
+		pTXRXDesc->control = 0x80000000 | (ENET_MAXF_SIZE - 1);
+		pRXStatusL->statusinfo = 0;
+		pRXStatusL->statushashcrc = 0;
+
+		// Save virtual address of buffer
+#ifdef USE_IRAM_FOR_ETH_BUFFERS
+		pRXVBuffs [idx] = (unsigned long) pbase3;
+#else
+#pragma error "NOT supported"
+#endif
+
+		// Next descriptor and status
+		pTXRXDesc++;
+		pRXStatusL++;
+		pbase1 += sizeof (TXRX_DESC_T);
+		pbase2 += sizeof (unsigned long);
+		pbase3 += ENET_MAXF_SIZE;
+	}
+
+    return 1;
+}
+
+//------------------------------------------------------------------------------
+int HWInit(bd_t * bd)
+{
+	int btemp;
+	unsigned int duplex, speed;
+	unsigned long tmp1, mst = 250;
+	int val = 0;
+
+	// Enable MAC interface
+	CLKPWR->clkpwr_macclk_ctrl = (CLKPWR_MACCTRL_HRCCLK_EN |
+		CLKPWR_MACCTRL_MMIOCLK_EN | CLKPWR_MACCTRL_DMACLK_EN |
+#ifdef USE_PHY_RMII
+		CLKPWR_MACCTRL_USE_RMII_PINS);
+#else
+		CLKPWR_MACCTRL_USE_MII_PINS);
+#endif
+
+	// Set RMII management clock rate. This clock should be slower
+	// than 12.5MHz (for NXP PHYs only). For a divider of 28, the
+	// clock rate when HCLK is 150MHz will be 5.4MHz
+	ENETMAC->mcfg = MCFG_CLOCK_SELECT(MCFG_CLOCK_HOST_DIV_28);
+
+	// Reset all MAC logic
+	ENETMAC->mac1 = (MAC1_SOFT_RESET | MAC1_SIMULATION_RESET |
+		MAC1_RESET_MCS_TX | MAC1_RESET_TX | MAC1_RESET_MCS_RX |
+		MAC1_RESET_RX);
+	ENETMAC->command = (COMMAND_REG_RESET | COMMAND_TXRESET |
+		COMMAND_RXRESET);
+	msDelay(10);
+
+	// Initial MAC initialization
+	ENETMAC->mac1 = MAC1_PASS_ALL_RX_FRAMES;
+	ENETMAC->mac2 = (MAC2_PAD_CRC_ENABLE | MAC2_CRC_ENABLE);
+	ENETMAC->maxf = ENET_MAXF_SIZE;
+
+	// Maximum number of retries, 0x37 collision window, gap */
+	ENETMAC->clrt = (CLRT_LOAD_RETRY_MAX(0xF) |
+		CLRT_LOAD_COLLISION_WINDOW(0x37));
+	ENETMAC->ipgr = IPGR_LOAD_PART2(0x12);
+
+#ifdef USE_PHY_RMII
+	// RMII setup
+	ENETMAC->command = (COMMAND_RMII | COMMAND_PASSRUNTFRAME);
+	ENETMAC->supp = SUPP_RESET_RMII;
+	msDelay(10);
+#else
+	// MII setup
+	ENETMAC->command = COMMAND_PASSRUNTFRAME;
+#endif
+
+	//////////////////////////////////////////////////////////////////////////
+	// reset phy
+	// 
+
+	mst = 1000;
+	btemp = 1;
+	while(mst > 0)
+	{
+		/* Wait for Link status to set UP or Timeout */
+		if(phy_get_link_status() == 0) {
+			mst = 0;
+			btemp = 0;
+			printf("ENET:auto-negotiation complete\n");
+		}
+		else {
+			mst--;
+			msDelay(1);
+		}
+	}
+	if(btemp) {
+		printf("ENET:auto-negotiation failed.\n");
+		return 0;
+	}
+
+	/*
+	 *  Read Vendor specific PHY Ctrl/Status Register to determine
+	 *  Ethernet Configuration
+	 */
+	tmp1 = 0;
+	RMII_Read (LAN8700_PHY_STATUS,&tmp1);
+	val = (tmp1 & 0x1c) >> 2;
+	switch (val) {
+		case 1:
+			/* 10Mbps Half Duplex */
+			duplex = 0;
+			speed = 0;
+			break;
+		case 5:
+			/* 10Mbps Full Duplex */
+			duplex = 1;
+			speed = 0;
+			break;
+		case 2:
+			/* 100Mbps Half Duplex */
+			duplex = 0;
+			speed = 1;
+			break;
+		case 6:
+			/* 100Mbps Full Duplex */
+			duplex = 1;
+			speed = 1;
+			break;
+	}
+
+	// Configure Full/Half Duplex mode
+	if (duplex == 1)
+	{
+		// 10MBase full duplex is supported
+		ENETMAC->mac2 |= MAC2_FULL_DUPLEX;
+		ENETMAC->command |= COMMAND_FULLDUPLEX;
+		ENETMAC->ipgt = IPGT_LOAD(0x15);
+		printf("ENET:FULL DUPLEX\n");
+	}
+	else
+	{
+		ENETMAC->ipgt = IPGT_LOAD(0x12);
+		printf("ENET:HALF DUPLEX\n");
+	}
+
+	// Configure 100MBit/10MBit mode
+	if (speed == 1)
+	{
+		// 100MBase mode
+		ENETMAC->supp = SUPP_SPEED;
+		printf("ENET:100MBase\n");
+	}
+	else
+	{
+		// 10MBase mode
+		ENETMAC->supp = 0;
+		printf("ENET:10Base\n");
+	}
+
+	// Save station address
+	ENETMAC->sa [2] = (unsigned long) (bd->bi_enetaddr[0] | (bd->bi_enetaddr[1] << 8));
+	ENETMAC->sa [1] = (unsigned long) (bd->bi_enetaddr[2] | (bd->bi_enetaddr[3] << 8));
+	ENETMAC->sa [0] = (unsigned long) (bd->bi_enetaddr[4] | (bd->bi_enetaddr[5] << 8));
+
+	// Setup TX and RX descriptors
+	txrx_setup();
+
+	// Enable broadcast and matching address packets
+	ENETMAC->rxfliterctrl = (RXFLTRW_ACCEPTUBROADCAST |
+		RXFLTRW_ACCEPTPERFECT);
+
+	// Clear and enable interrupts
+	ENETMAC->intclear = 0xFFFF;
+	ENETMAC->intenable = 0;
+
+	// Enable receive and transmit mode of MAC ethernet core
+	ENETMAC->command |= (COMMAND_RXENABLE | COMMAND_TXENABLE);
+	ENETMAC->mac1 |= MAC1_RECV_ENABLE;
+
+	// Perform a 'dummy' send of the first ethernet frame with a size of 0
+	// to 'prime' the MAC. The first packet after a reset seems to wait
+	// until at least 2 packets are ready to go.
+	tmp1 = 0;
+	eth_send(&tmp1, 4);
+
+	return 1;
+}
+
+//------------------------------------------------------------------------------
+int HWDeInit(void)
+{
+	// Reset all MAC logic
+	ENETMAC->mac1 = (MAC1_SOFT_RESET | MAC1_SIMULATION_RESET |
+		MAC1_RESET_MCS_TX | MAC1_RESET_TX | MAC1_RESET_MCS_RX |
+		MAC1_RESET_RX);
+	ENETMAC->command = (COMMAND_REG_RESET | COMMAND_TXRESET |
+		COMMAND_RXRESET);
+	msDelay(2);
+
+	// Disable MAC clocks, but keep MAC interface active
+#ifdef USE_PHY_RMII
+	CLKPWR->clkpwr_macclk_ctrl = CLKPWR_MACCTRL_USE_RMII_PINS;
+#else
+	CLKPWR->clkpwr_macclk_ctrl = CLKPWR_MACCTRL_USE_MII_PINS;
+#endif
+
+	return 1;
+}
+
+void eth_halt (void)
+{
+	HWDeInit();
+}
+
+int eth_init (bd_t * bd)
+{
+	int rc;
+
+	// Set MAC address from hardware
+	bd->bi_enetaddr[0] = phyhwdesc.mac[0];	bd->bi_enetaddr[1] = phyhwdesc.mac[1];	bd->bi_enetaddr[2] = phyhwdesc.mac[2];	bd->bi_enetaddr[3] = phyhwdesc.mac[3];	bd->bi_enetaddr[4] = phyhwdesc.mac[4];	bd->bi_enetaddr[5] = phyhwdesc.mac[5];
+	printf ("\tHW MAC address:  "
+		"%02X:%02X:%02X:%02X:%02X:%02X\n",
+		bd->bi_enetaddr[0], bd->bi_enetaddr[1],
+		bd->bi_enetaddr[2], bd->bi_enetaddr[3],
+		bd->bi_enetaddr[4], bd->bi_enetaddr[5] );
+
+	rc = HWInit(bd);
+
+	// De-init if an error occurred
+	if (rc == 0)
+	{
+		printf ("ENET init failure\n");
+		HWDeInit();
+		return -1;
+	}
+
+	return 0;
+}
+
+/* Get a data block via Ethernet */
+int eth_rx (void)
+{
+	unsigned long idx, length;
+
+	// Determine if a frame has been received
+	length = 0;
+	idx = ENETMAC->rxconsumeindex;
+	if (ENETMAC->rxproduceindex != idx)
+	{
+		// Clear interrupt
+		ENETMAC->intclear = MACINT_RXDONEINTEN;
+
+		// Frame received, get size of RX packet
+		length = (pRXStatus[idx].statusinfo & 0x7FF);
+
+		/* Pass the packet up to the protocol layer */
+		if (length > 0)
+		{
+		        memcpy((void *) NetRxPackets[0], (void *) pRXVBuffs [idx], length);
+			NetReceive (NetRxPackets[0], (unsigned short) length);
+		}
+
+		// Return DMA buffer
+		idx++;
+		if (idx >= ENET_MAX_TX_PACKETS)
+		{
+			idx = 0;
+		}
+		ENETMAC->rxconsumeindex = (unsigned long) idx;
+	}
+
+	return (int) length;
+}
+
+/* Send a data block via Ethernet. */
+int eth_send (volatile void *packet, int length)
+{
+	unsigned long idx, cidx, fb;
+
+	// Determine number of free buffers and wait for a buffer if needed
+	fb = 0;
+	while (fb == 0)
+	{
+		idx = ENETMAC->txproduceindex;
+		cidx = ENETMAC->txconsumeindex;
+
+		if (idx == cidx)
+		{
+			// Producer and consumer are the same, all buffers are free
+			fb = ENET_MAX_TX_PACKETS;
+		}
+		else if (cidx > idx)
+		{
+			fb = (ENET_MAX_TX_PACKETS - 1) -
+				((idx + ENET_MAX_TX_PACKETS) - cidx);
+		}
+		else
+		{
+			fb = (ENET_MAX_TX_PACKETS - 1) - (cidx - idx);
+		}
+	}
+
+	// Update descriptor with new frame size
+	pTXDesc[idx].control = (length | 0x40000000);
+
+	// Move data to buffer
+	memcpy((void *) pTXVBuffs [idx], (void *) packet, length);
+
+	// Get next index for transmit data DMA buffer and descriptor
+	idx++;
+	if (idx >= ENET_MAX_TX_PACKETS)
+	{
+		idx = 0;
+	}
+	ENETMAC->txproduceindex = idx;
+
+	return 0;
+}
+
---
+++ board/smart3250/phy3250.c
@@ -0,0 +1,409 @@
+/*
+ * Copyright (C) 2008 by NXP Semiconductors
+ * All rights reserved.
+ *
+ * @Author: Kevin Wells
+ * @Descr: Phytec 3250 board support functions
+ *
+ * See file CREDITS for list of people who contributed to this
+ * project.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ * MA 02111-1307 USA
+ */
+
+#include <common.h>
+#include <configs/smart3250.h>
+#include <asm/mach-types.h>
+#include <lpc3250.h>
+#include <net.h>
+#include "smart3250_prv.h"
+		
+DECLARE_GLOBAL_DATA_PTR;
+
+/* Initialize NOR Flash configuration */
+#ifdef CONFIG_FLASH_CFI_LEGACY
+ulong board_flash_get_legacy (ulong base, int banknum, flash_info_t * info)
+{
+	/*
+	 * PHY3250 board contains two 16-bit SPANSION NOR flash
+	 * to make a single 32 bit NOR flash. 
+	 */
+	if (banknum == 0)
+	{     /* non-CFI boot flash */
+		info->portwidth = FLASH_CFI_32BIT;
+		info->chipwidth = FLASH_CFI_BY16;
+		info->interface = FLASH_CFI_X16;
+		return 1;
+	} else
+		return 0;
+}
+#endif
+
+/*
+ * Dummy function to handle errors for EABI incompatibility
+ */
+void raise(void)
+{
+}
+
+/*
+ * Dummy function to handle errors for EABI incompatibility
+ */
+void abort(void)
+{
+}
+
+void reset_timer (void)
+{
+	unsigned int clkdlycnt, tbaseclk;
+
+	/* Reset timer */
+	TIMER_CNTR0->tcr = TIMER_CNTR_TCR_RESET;
+	TIMER_CNTR0->tcr = 0;
+	TIMER_CNTR0->tc = 0;
+
+	/* Clear and enable match function */
+	TIMER_CNTR0->ir = TIMER_CNTR_MTCH_BIT(0);
+
+	/* Count mode is PCLK edge */
+	TIMER_CNTR0->ctcr = TIMER_CNTR_SET_MODE(TIMER_CNTR_CTCR_TIMER_MODE);
+
+	/* Set prescale counter value for a 1mS tick */
+	tbaseclk = sys_get_rate(CLKPWR_PERIPH_CLK);
+	clkdlycnt = (tbaseclk / CONFIG_SYS_HZ);
+	TIMER_CNTR0->pr = clkdlycnt - 1;
+
+	/* Enable the timer */
+	TIMER_CNTR0->tcr = TIMER_CNTR_TCR_EN;
+}
+
+ulong get_timer (ulong base)
+{
+	ulong tcr = TIMER_CNTR0->tc;
+	return tcr - base;
+}
+
+void timer_init(void)
+{
+	/* Enable timer system clock */
+	CLKPWR->clkpwr_timers_pwms_clk_ctrl_1 |=
+		CLKPWR_TMRPWMCLK_TIMER0_EN;
+
+	reset_timer();
+}
+
+void udelay(unsigned long usec)
+{
+	unsigned long tbaseclk;
+
+	/* Enable timer system clock */
+	CLKPWR->clkpwr_timers_pwms_clk_ctrl_1 |=
+		CLKPWR_TMRPWMCLK_TIMER1_EN;
+
+	/* Reset timer */
+	TIMER_CNTR1->tcr = TIMER_CNTR_TCR_RESET;
+	TIMER_CNTR1->tcr = 0;
+	TIMER_CNTR1->tc = 0;
+	TIMER_CNTR1->pr = 0;
+
+	/* Clear and enable match function */
+	TIMER_CNTR1->ir = TIMER_CNTR_MTCH_BIT(0);
+
+	/* Count mode is PCLK edge */
+	TIMER_CNTR1->ctcr = TIMER_CNTR_SET_MODE(TIMER_CNTR_CTCR_TIMER_MODE);
+
+	/* Set prescale counter value for a 1uS tick */
+	tbaseclk = sys_get_rate(CLKPWR_PERIPH_CLK);
+	tbaseclk = (tbaseclk / 1000000) - 1;
+	TIMER_CNTR1->pr = tbaseclk;
+
+	/* Set match for number of usecs */
+	TIMER_CNTR1->mr[0] = usec;
+
+	/* Interrupt on match 0 */
+	TIMER_CNTR1->mcr = TIMER_CNTR_MCR_MTCH(0);
+
+	/* Enable the timer */
+	TIMER_CNTR1->tcr = TIMER_CNTR_TCR_EN;
+
+	/* Loop until match occurs */
+	while ((TIMER_CNTR1->ir & TIMER_CNTR_MTCH_BIT(0)) == 0);
+
+	/* Stop timer */
+	TIMER_CNTR1->tcr = 0;
+
+	/* Disable timer system clock */
+	CLKPWR->clkpwr_timers_pwms_clk_ctrl_1 &=
+		~CLKPWR_TMRPWMCLK_TIMER1_EN;
+}
+
+/*
+ * Miscellaneous platform dependent initialisations
+ */
+int misc_init_r(void)
+{
+	char enetaddr[18];
+	char *addr;
+	int i = 0;
+
+	sprintf(enetaddr, "%02X:%02X:%02X:%02X:%02X:%02X",
+			phyhwdesc.mac[0],
+			phyhwdesc.mac[1],
+			phyhwdesc.mac[2],
+			phyhwdesc.mac[3],
+			phyhwdesc.mac[4],
+			phyhwdesc.mac[5]);
+	setenv("ethaddr",enetaddr);
+
+	return 1;
+}
+
+PHY_HW_T phyhwdesc;
+
+static int ssp_read(void *buffer,
+			int max_fifo)
+{
+	volatile unsigned long tmp1;
+	int count = 0;
+	u_char *data8 = (u_char *) buffer;
+
+	while ((max_fifo > 0) && ((SSP0->sr & SSP_SR_RNE) != 0))
+	{
+		tmp1 = SSP0->data;
+		*data8 = (u_char) tmp1;
+		data8++;
+
+		/* Increment data count and decrement buffer size count */
+		count++;
+		max_fifo--;
+	}
+
+	return count;
+}
+
+static int ssp_write(void *buffer,
+                 int n_fifo)
+{
+	int count = 0;
+	u_char *data8 = (u_char *) buffer;
+
+	/* Loop until transmit ring buffer is full or until n_bytes
+	   expires */
+	while ((n_fifo > 0) && ((SSP0->sr & SSP_SR_TNF) != 0))
+	{
+		SSP0->data = (unsigned long) * data8;
+		data8++;
+
+		/* Increment data count and decrement buffer size count */
+		count++;
+		n_fifo--;
+	}
+
+	return count;
+}
+
+static void ssp_set_clock(unsigned long target_clock)
+{
+	unsigned long control, prescale, cr0_div, cmp_clk, ssp_clk;
+
+	/* Get clock for SSP */
+	ssp_clk = sys_get_rate(CLKPWR_HCLK);
+
+	/* Find closest divider to get at or under the target frequency.
+	   Use smallest prescaler possible and rely on the divider to get
+	   the closest target frequency */
+	cr0_div = 0;
+	cmp_clk = 0xFFFFFFFF;
+	prescale = 2;
+	while (cmp_clk > target_clock)
+	{
+		cmp_clk = ssp_clk / ((cr0_div + 1) * prescale);
+		if (cmp_clk > target_clock)
+		{
+			cr0_div++;
+			if (cr0_div > 0xFF)
+			{
+				cr0_div = 0;
+				prescale += 2;
+			}
+		}
+	}
+
+	/* Write computed prescaler and divider back to register */
+	control = SSP0->cr0 &= ~(SSP_CR0_SCR(0xFF));
+	SSP0->cr0 = control | SSP_CR0_SCR(cr0_div - 1);
+	SSP0->cpsr = prescale;
+}
+
+/*
+ * Initialize SSP0 for interface to the serial EEPROM
+ */
+static void smart3250_ssp0_init(void)
+{
+	volatile unsigned long tmp;
+
+	/* Enable SSP0 clock */
+	CLKPWR->clkpwr_ssp_blk_ctrl |= CLKPWR_SSPCTRL_SSPCLK0_EN;
+
+	SSP0->cr0 = (SSP_CR0_DSS(8) | SSP_CR0_FRF_SPI);
+	SSP0->cr1 |= SSP_CR1_SSP_ENABLE;
+
+	/* Empty FIFO */
+	while ((SSP0->sr & SSP_SR_RNE) != 0)
+	{
+		tmp = SSP0->data;
+	}
+
+	ssp_set_clock(500000);
+
+	/* Clear latched interrupts */
+	SSP0->icr = (SSP_ICR_RORIC | SSP_ICR_RTIC);
+
+	/* Disable interrupts */
+	SSP0->imsc = 0; // TBD (SSP_IMSC_RORIM | SSP_IMSC_RTIM | SSP_IMSC_RXIM);
+}
+
+static int smart3250_sspxfer(u_char *out,
+                               u_char *in,
+                               int bytes)
+{
+  int rbytes = 0, xfrd = 0;
+
+	/* Assert chip select */
+	GPIO->p3_outp_clr = OUTP_STATE_GPIO(5);
+	ssp_write(out, bytes);
+	while (rbytes < bytes)
+	{
+		rbytes += ssp_read(&in [rbytes], 1);
+	}
+
+	GPIO->p3_outp_set = OUTP_STATE_GPIO(5);
+	xfrd = 1;
+
+	return xfrd;
+}
+
+static u_char smart3250_sspread(int index)
+{	
+	u_char datai [8], datao [8];
+	u_char byte = 0;
+
+	/* Read byte */
+	datao [0] = SEEPROM_READ;
+	datao [1] = (u_char)((index >> 8) & 0xFF);
+	datao [2] = (u_char)((index >> 0) & 0xFF);
+	datao [3] = 0xFF;
+	smart3250_sspxfer(datao, datai, 4);
+	byte = datai [3];
+
+	return byte;
+}
+
+void smart3250_get_board_info(void)
+{
+	u_char *p8;
+	int idx;
+
+	/* Initialize SSP0 */
+	smart3250_ssp0_init();
+
+	/* Read data from EEPROM - this needs to be done here as the
+	   SDRAM configuration depends on these settings. */
+	p8 = (u_char *) & phyhwdesc;
+	for (idx = 0; idx < sizeof(phyhwdesc); idx++)
+	{
+		*p8 = smart3250_sspread(SMART3250_SEEPROM_CFGOFS + idx);
+		p8++;
+	}
+
+	if (phyhwdesc.fieldvval != PHY_HW_VER_VAL)
+	{
+		/* Set some defaults */
+		phyhwdesc.dramcfg = (PHYHW_DRAM_TYPE_LPSDRAM | PHYHW_DRAM_SIZE_64M);
+		phyhwdesc.syscfg = PHYHW_SDIO_POP;
+		phyhwdesc.fieldvval = PHY_HW_VER_VAL;
+
+		/* Default MAC address in index order of 0:1:2:3:4:5 */
+		phyhwdesc.mac [0] = 0x00;
+		phyhwdesc.mac [1] = 0x01;
+		phyhwdesc.mac [2] = 0x90;
+		phyhwdesc.mac [3] = 0x00;
+		phyhwdesc.mac [4] = 0xC0;
+		phyhwdesc.mac [5] = 0x81;
+		phyhwdesc.mac [6] = 0x00;
+		phyhwdesc.mac [7] = 0x00;
+	}
+}
+
+int board_init (void)
+{	
+	smart3250_get_board_info();
+
+	/* arch number of Logic-Board - MACH_TYPE_LPC3XXX */
+	gd->bd->bi_arch_number = MACH_TYPE_UBOOTSYS;
+
+	/* adress of boot parameters */
+	gd->bd->bi_boot_params = CONFIG_ENV_ADDR;
+
+#ifdef CONFIG_SYS_FLASH_CFI
+	/* Use 32-bit memory interface for NOR Flash */
+	EMC->emcstatic_regs[0].emcstaticconfig = 0x82;
+	/* 
+	 * After Setting a higher clock speed, change the NOR timings to 
+	 * optimum value to get maximum bandwidth
+	 */
+	EMC->emcstatic_regs[0].emcstaticwaitwen = 0x0;
+	EMC->emcstatic_regs[0].emcstaticwait0en = 0x0;
+	EMC->emcstatic_regs[0].emcstaticwaitrd = 0xb;
+	EMC->emcstatic_regs[0].emcstaticpage = 0xb;
+	EMC->emcstatic_regs[0].emcstaticwr = 0x3;
+	EMC->emcstatic_regs[0].emcstaticturn = 0x1;
+#endif
+
+	return 0;
+}
+
+int dram_init (void)
+{
+	unsigned long sz = phyhwdesc.dramcfg & PHYHW_DRAM_SIZE_MASK;
+
+	gd->bd->bi_dram[0].start = PHYS_SDRAM_1;
+
+	if (sz == PHYHW_DRAM_SIZE_16M)
+	{
+		/* 32M available */
+		gd->bd->bi_dram[0].size = 0x01000000; /* 16M */
+	}
+	else if (sz == PHYHW_DRAM_SIZE_32M)
+	{
+		/* 32M available */
+		gd->bd->bi_dram[0].size = 0x02000000; /* 32M */
+	}
+	else if (sz == PHYHW_DRAM_SIZE_64M)
+	{
+		/* 64M available */
+		gd->bd->bi_dram[0].size = 0x04000000; /* 64M */
+	}
+	else
+	{
+		/* 128M available */
+		gd->bd->bi_dram[0].size = 0x08000000; /* 128M */
+	}
+
+	return 0;
+}
+
+
